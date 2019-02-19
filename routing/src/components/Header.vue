d54f379e6db530ba5a3c7387d68bbcb0b5b45fc0
Jump1556 <anastasiia.kostashenko@gmail.com>
2019-02-19 09:55:21 +0100

    Commiting a project Routing in vuejs application

---
+++ routing/src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <ul class="nav nav-pills">
+  <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
+  <!-- exact is to make sure path is "/" not any started with "/" -->
+  <router-link to="/user" tag="li" active-class="active"><a>User</a></router-link>
+</ul>
+</template>
+
+
