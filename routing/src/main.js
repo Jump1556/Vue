3e4000226347b32bc7e136ba008ebd87d25926b5
Jump1556 <anastasiia.kostashenko@gmail.com>
2019-02-19 11:46:16 +0100

    Passing the hash fragment and controll scroll behavior

--- routing/src/main.js
+++ routing/src/main.js
@@ -6,7 +6,16 @@ import { routes } from './routes'
 Vue.use(VueRouter);
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    if (to.hash) {
+      return { selector: to.hash };
+    }
+    return {x:0, y: 700};
+  }
 });
 // default mode: hash than you see in url example.com/#/user
 
