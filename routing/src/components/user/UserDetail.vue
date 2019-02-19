5c52d543ee3a5fcfaee7ed9c8b69452c36ae7fee
Jump1556 <anastasiia.kostashenko@gmail.com>
2019-02-19 16:58:45 +0100

    Using beforeEnter and beforeLeave guard to control users routing

--- routing/src/components/user/UserDetail.vue
+++ routing/src/components/user/UserDetail.vue
@@ -4,9 +4,32 @@
     <p>User loaded has ID: {{ $route.params.id }}</p>
     <router-link 
       tag="button" 
-      :to="{ name: 'userEdit', params: { id: $route.params.id}, query: { locale:'en', q:100 }}"   
+      :to="link"  
       class="btn btn-primary">Edit User</router-link>
     <!-- additional parameter to :to after id, can be query: { locale:'en', q:100 }-->
   </div>
     
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      link: { 
+        name: 'userEdit',
+        params: { id: this.$route.params.id},
+        query: { locale:'en', q:100 },
+        hash: '#data'
+      }
+    }
+  },
+  // in special case you need to use:
+  // beforeRouteEnter(to, from, next) { 
+  //   if (true) to check authentication or so on {
+  //     next(vm => { vm.link});
+  //  } else {
+  //      next(false);
+  //  }
+  //}
+}
+</script>
